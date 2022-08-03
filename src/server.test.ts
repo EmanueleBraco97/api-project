4a39dff4b655915ecd819367d46f15fc4f6b264b
Emanuele <emanuelestudent@LAPTOP-V6S8LTL2.localdomain>
2022-08-03 10:58:22 +0200

    add post /planets route

--- src/server.test.ts
+++ src/server.test.ts
@@ -6,38 +6,23 @@ import app from './app';
 
 const request = supertest(app);
 
-test("GET /planets", async () => {
-    const planets = [
+test("POST /planets", async () => {
+    const planet = 
         {
             id: 1,
             name: "Mercury",
             description: null,
             diametr: 1234,
             moons: 12,
-            createdAt: "2022-08-01T14:14:42.931Z",
-            updatedAt: "2022-08-01T14:09:40.079Z"
-        },
-        {
-            id: 2,
-            name: "Venus",
-            description: null,
-            diametr: 5668,
-            moons: 10,
-            createdAt: "2022-08-01T14:14:42.931Z",
-            updatedAt: "2022-08-01T14:10:45.551Z"
         }
-    ];
-
-    // @ts-ignore
-    prismaMock.planet.findMany.mockResolvedValue(planets);
-
 
     const response = await request
-        .get("/planets")
-        .expect(200)
-        .expect("Content-Type", /application\/json/)
+        .post("/planets")
+        .send(planet)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
     
-    expect(response.body).toEqual(planets)
+    expect(response.body).toEqual(planet)
 
 
 });
